5892bdceb4509274d431d6b04345e00a9b0a6673
Tarkan TIKIS <contact@kanform.fr>
2023-05-16 17:12:26 +0200

    integration network tools

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ source torsocks on
 url="$(shuf -n 1 /usr/local/src/url-list)"
 echo "$(date '+%Y-%m-%d %H:%M:%S') Trying to wget ${url}"
 
-wget -qO - ${url}; echo >> /var/log/tor/request.log
+wget -qO - ${url} >> /var/log/tor/request.log
 
 sleep 3600
 
